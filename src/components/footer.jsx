97f790616f0486a09dc9a24b2f079d260d4810e8
HarlabE <olagunjuayodeleganiyu@gmail.com>
2025-11-10 18:02:41 +0100

    style: fix footer style

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <footer style={{margin: '0'}}>
-			<div className="container">
-				<p className='text-center'>Copy Right &copy; Izin Akioya | All Right Reserved.</p>
+        <footer>
+			<div className="">
+				<p>Copy Right &copy; Izin Akioya | All Right Reserved.</p>
 			</div>
 		</footer>
     </div>
